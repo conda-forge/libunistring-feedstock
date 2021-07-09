cee7f910fab1673f5d98ce4d23024843db9490c7
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-07-09 06:47:46 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,10 +5,10 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./build-aux
 ./configure --disable-dependency-tracking --disable-silent-rules --prefix=${PREFIX}
 make
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
-if [ ${target_platform} == linux-* ]; then
-  make check || true
-else
-  make check
-fi
+  if [[ "${target_platform}" == linux-* ]]; then
+    make check || true
+  else
+    make check
+  fi
 fi
 make install
